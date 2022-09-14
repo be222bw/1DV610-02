2d2e8ea930ddb47e5b3bdcffdf2f607393f80f6d
Björn Elmqvist <be222bw@student.lnu.se>
2022-09-14 14:20:41 +0200

    Create window-template.js.

---
+++ src/js/components/WindowElement/window-template.js
@@ -0,0 +1,11 @@
+export default
+`<link rel="stylesheet" type="text/css" href="./Window-Style.css">
+<border-element>
+  <title-bar>
+    <div id="buttoncontainer">
+      <minimise-button></minimise-button>
+      <maximise-button></maximise-button>
+      <close-button></close-button>
+    </div>
+  </title-bar>
+</border-element>`
