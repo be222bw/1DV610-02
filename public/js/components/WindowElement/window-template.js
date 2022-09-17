a13a2f1f702c96647c7366222a8f90b7ed525e68
Björn Elmqvist <be222bw@student.lnu.se>
2022-09-17 16:39:24 +0200

    Fix hovering CSS.

--- public/js/components/WindowElement/window-template.js
+++ public/js/components/WindowElement/window-template.js
@@ -3,9 +3,9 @@ export default
 <border-element>
   <title-bar>
     <div id="buttoncontainer">
-      <minimise-button></minimise-button>
-      <maximise-button></maximise-button>
-      <close-button></close-button>
+      <minimise-button class="button"></minimise-button>
+      <maximise-button class="button"></maximise-button>
+      <close-button class="button"></close-button>
     </div>
   </title-bar>
 </border-element>`
