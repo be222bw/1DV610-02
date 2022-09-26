57549bce903b3d0a298dcbbdf9fcb285f6218bce
Björn Elmqvist <be222bw@student.lnu.se>
2022-09-26 19:36:08 +0200

    Center buttons.

--- public/js/components/TitleBar/titlebar-template.js
+++ public/js/components/TitleBar/titlebar-template.js
@@ -1,8 +1,8 @@
 export default `
 <link rel="stylesheet" type="text/css" href="js/components/TitleBar/titlebar-template.css">
 <div id="buttoncontainer">
-  <minimise-button class="button"><div>X</div></minimise-button>
+  <minimise-button class="button"><div>_</div></minimise-button>
   <maximise-button class="button"><div>&squ;</div></maximise-button>
-  <close-button class="button"><div>_</div></close-button>
+  <close-button class="button"><div>X</div></close-button>
 </div>
 `
